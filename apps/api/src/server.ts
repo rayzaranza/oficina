7e2ae8601ab943cffd9c3629e4b9ef8a57fc32ba
rayzaranza <rayzaranza@gmail.com>
2026-07-15 20:47:24 -0300

    refactor: use factory function to create fastify instance

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,7 +1,10 @@
-import { app } from "./app.js";
+import { buildFastify } from "./app.js";
+
+const app = buildFastify({ logger: true });
 
 app.listen({ port: 3000, host: "0.0.0.0" }, (error) => {
   if (error) {
-    console.error(error);
+    app.log.error(error);
+    process.exit(1);
   }
 });
